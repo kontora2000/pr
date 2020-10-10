553757f2629261000cf33f07b47e106c63aabad3
stasmyhero <stasmyhero@gmail.com>
2020-10-10 14:03:47 +0300

    a'

---
+++ components/MarqueeBackgroundRow.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="title-background-row-wrapper">
+    <span class="title-background-row" ref="row"><slot /><span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MarqueBackgroundRow',
+  props: {
+      shift: {
+          type: String,
+          default: '0vw'
+      }
+  },
+  data () {
+    return {
+      isAnimated: true,
+      timeline: null
+    }
+  },
+  mounted () {
+
+  }
+}
+</script>
+
+<style>
+    @keyframes marquee{
+        0% {text-indent: 100%;}
+        100% {text-indent: -100%;}
+        }
+    }
+
+    .title-background-row-wrapper {
+        position: relative;
+    }
+
+    .title-background-row {
+        animation: marquee 10s infinite;
+    }
+</style>
