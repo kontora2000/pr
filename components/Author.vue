ed1d1eba4606fa8613716b6f14555e8a0d787a71
stasmyhero <stasmyhero@gmail.com>
2020-12-01 23:24:15 +0300

    added sections for authors

---
+++ components/Author.vue
@@ -0,0 +1,16 @@
+<template>
+  <a :href="author.url">
+    Имя {{ author.name }} {{ author.pos }}
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    author: {
+      type: Object,
+      required: true,
+    },
+  },
+}
+</script>
---
+++ components/sections/Gallery.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="realised-gallery-grid gallery-grid">
+    <picture v-for="(image, index) in gallery" :key="index" class="gallery-photo-wrapper">
+      <img class="gallery-photo" :src="image.src" @click="$emit('openlightbox', image)">
+    </picture>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Gallery',
+  data () {
+    return {
+
+      gallery: [
+        {
+          src: '/projects/1.jpg',
+          a: [
+            { name: 'Автор 1', pos: 'Поз 2', url: 'http://www.asd.ru', }
+          ],
+        },
+        {
+          src: '/projects/2.jpg',
+          a: [
+            { name: 'Автор 2', pos: 'Поз 2', url: 'http://www.asd.ru', },
+            { name: 'Автор 22', pos: 'Поз 22', url: 'http://www.asd22.ru', }
+          ],
+        },
+        {
+          src: '/projects/3.jpg',
+          a: {
+            name: 'Автор 3',
+            position: 'Поз 3',
+            url: 'http://www.asd3.ru',
+          },
+        }
+      ], // end gallery
+    }
+  },
+}
+</script>
